81afcb19bc2fc4c69d1368b019f1b35d5c15caba
konyka <konyka@126.com>
2019-04-06 20:03:18 +0800

    mkdir ...

---
+++ src/server/dbmgr/main.cpp
@@ -0,0 +1,32 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: main.cpp
+ *      Version: v0.0.0
+ *   Created on: Apr 5, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    puts("!!!Hello World!!!"); 
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
